0bc60c88b8834c7e6dfd66201ab0f59294fc4d50
André Krämer <andre.kraemer@qualitybytes.de>
2023-04-14 14:40:27 +0200

    Added an additional test

--- Ak.MemoryLeakDemo.Tests/Demo1Tests.cs
+++ Ak.MemoryLeakDemo.Tests/Demo1Tests.cs
@@ -1,13 +1,9 @@
 ﻿using Ak.MemoryLeakDemo.ViewModels;
 using JetBrains.dotMemoryUnit;
-using JetBrains.dotMemoryUnit.Kernel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
+using JetBrains.dotMemoryUnit.Properties;
 using NUnit.Framework;
 
 
@@ -17,24 +13,35 @@ namespace Ak.MemoryLeakDemo.Tests
     [Apartment(ApartmentState.STA)]
     public class Demo1Tests
     {
+        private App _app;
 
         [OneTimeSetUp]
         public void TestFixtureSetup()
         {
-            var app = new App();
-            app.InitializeComponent();
+            _app = new App();
+            _app.InitializeComponent();
+          //  MainWindow window = new MainWindow();
         }
 
 
         [Test]
         [DotMemoryUnit(FailIfRunWithoutSupport = false, SavingStrategy = SavingStrategy.OnAnyFail)]
-        public void Demo1Test()
+        public void Window1_ShouldNotBeInMemory()
         {
+
+            //var window = new MainWindow();
+
+            //var checkpoint = dotMemory.Check();
 
             // Öffnet das Fenster Demo1Window und schließt es direkt wieder
             // Das öffnen muss in einer separaten Methode erledigt werden, damit die Testmethode
             // keine lokale Variable vom Typ Demo1Window hält. Dies würde sonst das Ergebnis verfälschen
             CreateAndCloseWindow1();
+
+            //dotMemory.Check(memory =>
+            //    Assert.That(memory.GetDifference(checkpoint)
+            //        .GetSurvivedObjects(where => where.Type.Is<Logfile>())
+            //        .ObjectsCount, Is.EqualTo(0)));
 
             // Prüft, ob kein LogFile Objekt mehr im Speicher ist.
             // LogFile Objekte werden innerhalb von Demo1Window erzeugt. 
@@ -49,6 +56,25 @@ namespace Ak.MemoryLeakDemo.Tests
                 Assert.That(memory.GetObjects(where => where.Type.Is<Demo1Window>())
                                   .ObjectsCount, 
                             Is.EqualTo(0)));
+        }
+
+        [Test]
+        [DotMemoryUnit(FailIfRunWithoutSupport = false, SavingStrategy = SavingStrategy.OnAnyFail)]
+        public void NoByteArrays_ShuoldBeInLOH()
+        {
+            // Öffnet das Fenster Demo1Window und schließt es direkt wieder
+            // Das öffnen muss in einer separaten Methode erledigt werden, damit die Testmethode
+            // keine lokale Variable vom Typ Demo1Window hält. Dies würde sonst das Ergebnis verfälschen
+            CreateAndCloseWindow1();
+
+            // Prüft, ob kein ByteArray im Large Object Heap (LOH)
+            // LogFile Objekte werden innerhalb von Demo1Window erzeugt. 
+            // Wenn Demo1Window nicht korrekt zerstört wird, z. B. weil der WPF Dispatcher nicht lief, oder weil die registrierten
+            // Events nicht abgemeldet wurden, dann schlägt dieser Test fehl.
+            dotMemory.Check(memory =>
+            Assert.That(memory.GetObjects(where => where.Type.Is<Byte[]>())
+                               .GetObjects(where => where.Generation.Is(Generation.LOH))
+                        .ObjectsCount, Is.EqualTo(0)));
         }
 
         private void CreateAndCloseWindow1()
